8e26a55445594fecedd125180a4698b7c774252a
Eva Veshtort <eva.veshtort@gmail.com>
2024-11-18 22:51:14 +0300

    mock for details pages added

--- stat_calc_app/src/pages/metricDetailsPage.tsx
+++ stat_calc_app/src/pages/metricDetailsPage.tsx
@@ -5,6 +5,8 @@ import { Metric, getMetricById } from "../modules/StatisticianApi";
 import { Spinner, Image } from "react-bootstrap";
 import BasePage from "./BasePage";
 import { ROUTES, ROUTE_LABELS } from "../../Routes.tsx";
+import { METRICS_MOCK } from "../modules/mock";
+import image from "../components/DefaultImage.png";
 
 export const MetricDetailsPage: FC = () => {
   const [pageData, setPageDdata] = useState<Metric>();
@@ -12,7 +14,13 @@ export const MetricDetailsPage: FC = () => {
 
   useEffect(() => {
     if (!id) return;
-    getMetricById(id).then((response) => setPageDdata(response));
+    getMetricById(id)
+    .then((response) => setPageDdata(response))
+    .catch(() => {
+      setPageDdata(
+        METRICS_MOCK.metrics.filter((item) =>
+          item.metric_id == Number(id))[0])}
+        )
   }, [id]);
 
   return (
@@ -30,12 +38,12 @@ export const MetricDetailsPage: FC = () => {
                 <div className="metricTitle">{pageData.title}</div>
                 <div className="infoSpace">
                   <Image
-                    src={pageData.picture_url}
+                    src={pageData.picture_url || image}
                     alt="card"
                     height={200}
                     style={{ marginRight: "20px" }}
                   />
-                  <p>{pageData.description}</p>
+                  <p>{pageData.description || "Описание метрики"}</p>
                 </div>
               </div>
             </BasePage>
