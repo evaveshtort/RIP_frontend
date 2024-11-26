38cd983227c56b3509a6984ef6095100e3cb816b
Eva Veshtort <eva.veshtort@gmail.com>
2024-11-26 15:01:26 +0300

    routing changed

--- stat_calc_app/src/App.tsx
+++ stat_calc_app/src/App.tsx
@@ -1,18 +1,21 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { MetricDetailsPage } from "./pages/metricDetailsPage";
 import { HomePage } from "./pages/HomePage";
 import MetricsListPage from "./pages/metricsListPage";
 import { ROUTES } from "../Routes";
 
 function App() {
+
+  const basename = import.meta.env.PROD ? "/statistician-frontend" : "/";
+
     return (
-      <BrowserRouter basename="/statistician-frontend">
+      <HashRouter basename={basename}>
         <Routes>
           <Route path={ROUTES.HOME} index element ={<HomePage />}/>
           <Route path={ROUTES.METRICS} index element={<MetricsListPage />}/>
           <Route path={`${ROUTES.METRICS}/:id`} index element={<MetricDetailsPage />}/>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     );
   }
   
--- stat_calc_app/vite.config.ts
+++ stat_calc_app/vite.config.ts
@@ -2,17 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: { 
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, "/")
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === 'production' ? '/statistician-frontend/' : '/', 
+    server: { 
+      port: 3000,
+      proxy: {
+        "/api": {
+          target: "http://localhost:8000",
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, "/")
+        },
       },
-    },
-   },
-  plugins: [react()],
-  base: "/statistician-frontend"
-})
+     },
+  };
+});
